37c5ea90bffa0d58507bada6c83c16ecc11fbfda
patrick leal <pjleal3846@icloud.com>
2020-01-12 04:22:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,9 @@
  def using_push
    colors_in_the_rainbow.push(next_color)
  end
-  def updated_array.first(new_neighborhood)
+  def using_unshift
+    updated_array.first(new_neighborhood)
+  end
+  def using_size
   updated_array.size
+end
